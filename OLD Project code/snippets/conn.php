1d60e0f76bcc06d4422652a2dbf5df1e5c570b84
alexrjgrant <alexrjgrant@hotmail.co.uk>
2020-01-10 07:40:28 +0000

    old code Zipped

--- OLD Project code/snippets/conn.php
+++
@@ -1,1 +0,0 @@
-<?php  $conn= new PDO("mysql:host=localhost;dbname=agrant;","agrant","Eid8Zaam"); ?>
--- OLD Project code/wsDelete.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$shiftID = $_POST["shiftID"];
-
-$shiftQ = $conn->prepare("DELETE FROM SHIFTS WHERE ShiftID = :sID");
-$shiftQ -> bindParam(":sID", $shiftID);
-$shiftQ -> execute();
-
-?>
